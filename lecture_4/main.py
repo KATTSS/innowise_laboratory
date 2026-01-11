22d54e81d3e298e8b54a944eae7661aa7b572cba
KATTSS <katsyarynnks@gmail.com>
2026-01-11 21:57:32 +0300

    create school.db with students, grades tables

--- lecture_4/main.py
+++ lecture_4/main.py
@@ -1,1 +1,17 @@
+import sqlite3
+with sqlite3.connect('school.db') as connection:
+
+    cursor= connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS students ("
+                   "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                   "full_name TEXT NOT NULL,"
+                   "birth_year INTEGER NOT NULL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS grades ("
+                   "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                   "student_id INTEGER NOT NULL,"
+                   "subject TEXT NOT NULL,"
+                   "grade INTEGER NOT NULL,"
+                   "FOREIGN KEY (student_id) REFERENCES students(id)"
+                   ");")
+
 
